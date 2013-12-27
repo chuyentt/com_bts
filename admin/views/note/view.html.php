5f05d48893aa365a14a1d8705621fbad90d8c1b2
chuyentt <chuyentrungtran@Chuyens-MacBook-Pro.local>
2013-12-27 11:46:56 +0700

    Update toolbar
    Bỏ các chức năng edit khi view note

--- admin/views/note/view.html.php
+++ admin/views/note/view.html.php
@@ -61,15 +61,15 @@ class BtsViewNote extends JViewLegacy
 		if (!$checkedOut && ($canDo->get('core.edit')||($canDo->get('core.create'))))
 		{
 
-			JToolBarHelper::apply('note.apply', 'JTOOLBAR_APPLY');
-			JToolBarHelper::save('note.save', 'JTOOLBAR_SAVE');
+			//JToolBarHelper::apply('note.apply', 'JTOOLBAR_APPLY');
+			//JToolBarHelper::save('note.save', 'JTOOLBAR_SAVE');
 		}
 		if (!$checkedOut && ($canDo->get('core.create'))){
-			JToolBarHelper::custom('note.save2new', 'save-new.png', 'save-new_f2.png', 'JTOOLBAR_SAVE_AND_NEW', false);
+			//JToolBarHelper::custom('note.save2new', 'save-new.png', 'save-new_f2.png', 'JTOOLBAR_SAVE_AND_NEW', false);
 		}
 		// If an existing item, can save to a copy.
 		if (!$isNew && $canDo->get('core.create')) {
-			JToolBarHelper::custom('note.save2copy', 'save-copy.png', 'save-copy_f2.png', 'JTOOLBAR_SAVE_AS_COPY', false);
+			//JToolBarHelper::custom('note.save2copy', 'save-copy.png', 'save-copy_f2.png', 'JTOOLBAR_SAVE_AS_COPY', false);
 		}
 		if (empty($this->item->id)) {
 			JToolBarHelper::cancel('note.cancel', 'JTOOLBAR_CANCEL');
